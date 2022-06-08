90d6ed63de02ee708f18cfda28719b5f68cbe216
tuanio <nvatuan3@gmail.com>
2022-06-08 15:24:02 +0700

    new deploy

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ import os
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app)
+app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "*"}}) # change from '*' to this route 
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from app.routes import *
