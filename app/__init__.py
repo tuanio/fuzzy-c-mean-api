184043708f7fd1d566594fa6272f8764703e9e49
Nguyen Van Anh Tuan <nvatuan3@gmail.com>
2022-06-22 12:06:15 +0700

    edit in app

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,19 @@ app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "https://fuzzy-c-mean-fe.vercel.app"}}) # change from '*' to this route 
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:e9Jx7sXB4UAW1rZHZOLF@containers-us-west-75.railway.app:7285/railway"
+
+username = os.environ["PGUSER"]
+password = os.environ["PGPASSWORD"]
+db_name = os.environ["PGDATABASE"]
+host = os.environ["PGHOST"]
+port = os.environ["PGPORT"]
+uri = os.environ["DATABASE_URL"]
+
+# local config database
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = f"postgresql://{username}:{password}@{host}:{port}/{db_name}"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:e9Jx7sXB4UAW1rZHZOLF@containers-us-west-75.railway.app:7285/railway"
 
 db = SQLAlchemy(app)
 
